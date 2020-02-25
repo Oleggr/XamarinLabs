6e6a7d00e4a8add917d47eef065f14cea808c828
Oleg Gritchin <oleg.gr@outlook.com>
2020-02-25 11:16:23 +0300

    lab 1 done

---
+++ Phoneword/Phoneword/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Phoneword
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // Get our UI controls from the loaded layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            
+            TextView sortedNumberField = FindViewById<TextView>(Resource.Id.SortedNumber);
+
+
+            // Add code to translate number
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                string sortedNumber = Core.SelectionSorter.SelectionSort(translatedNumber);
+
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                    sortedNumberField.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                    sortedNumberField.Text = sortedNumber;
+                }
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Phoneword/Phoneword/SelectionSort.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+namespace Core
+{
+    public static class SelectionSorter
+    {
+        public static string SelectionSort(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return "";
+            
+            List<char> numsList = new List<char>();
+
+            foreach (var c in raw)
+            {
+                if ("0123456789".Contains(c))
+                {
+                    numsList.Add(c);
+                }
+
+            }
+
+            char[] nums = numsList.ToArray();
+
+            Sort(nums);
+
+            string sortedNumber = new string(nums);
+
+            return sortedNumber;
+        }
+
+        private static int TranslateToNumber(char c)
+        {
+            return (int) Char.GetNumericValue(c);
+        }
+
+        private static void Sort(char[] arr)
+        {
+            int i, j, min;
+            for (i = 0; i < arr.Length; i++)
+            {
+                min = i;
+                for (j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] > arr[min])
+                    {
+                        min = j;
+                        Swap(ref arr[i], ref arr[min]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(ref char x, ref char y)
+        {
+            char temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
